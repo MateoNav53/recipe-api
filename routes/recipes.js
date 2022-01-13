3f22208c0b9c9fbfabd3c64e4d8c5e2ea1e9c012
Mateo Navarrette <mateonav24@gmail.com>
2022-01-12 20:17:45 -0800

    added route for getting individual recipe details + cleanup

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,15 +2,21 @@ const express = require('express');
 const router = express.Router();
 const data = require('./../data.json')
 
-
+//get all recipe names
 router.get("/", (req, res, next) => {
     const namesArr = []
     data.recipes.forEach((recipe) => namesArr.push(recipe.name))
-    res.status(200).send({recipeNames: namesArr})
+    res.status(200).json({recipeNames: namesArr})
 })
 
-
-
+//get the ingredients and number of steps for a recipe given a recipe name in req.params
+router.get("/details/:recipeName", (req, res) => {
+    const recipeDetails = data.recipes.find((recipe) => recipe.name === req.params.recipeName)
+    if(!recipeDetails){
+        res.json('Recipe not found, or spelled incorrectly')
+    }
+    res.status(200).json({details: {"ingredients": recipeDetails.ingredients, "numSteps": recipeDetails.instructions.length}})
+})
 
 
 
--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-//all you need to do to make a server is these 5 lines of code
 const express = require('express')
 const app = express();
 
-app.listen(3000, () => {
-    console.log("We live on port 3000")
+require('dotenv').config();
+
+const port = 3000 || process.env.PORT
+
+app.listen(port, () => {
+    console.log(`We live on port ${port}`)
 })
-///////////
 
-//all you need to do to run a get request
-//state your route and then res signifying response
-//followed by a status code, then your json response
-
-// app.use()
 app.use('/recipes', require('./routes/recipes'))
 
-// app.get("/recipes", (req, res, next) => {
-//     res.status(200).json(data.recipes)
-// })
-
-// app.post("/add", )
 
 
