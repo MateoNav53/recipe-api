954d488be100357d425c7e2f21e1c2598a39722a
Mateo Navarrette <mateonav24@gmail.com>
2022-01-14 16:27:53 -0800

    post requests work! data in data.json gets written

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const data = require('./../data.json')
+let data = require('../data.json')
+const fs = require('fs')
+
+//get all information on all recipes
+router.get("/all", (req, res, next) => {
+    res.status(200).json(data)
+})
 
 //get all recipe names
 router.get("/", (req, res, next) => {
@@ -15,9 +21,27 @@ router.get("/details/:recipeName", (req, res) => {
     if(!recipeDetails){
         res.json('Recipe not found, or spelled incorrectly')
     }
-    res.status(200).json({details: {"ingredients": recipeDetails.ingredients, "numSteps": recipeDetails.instructions.length}})
+    res.status(200).json({details: {ingredients: recipeDetails.ingredients, numSteps: recipeDetails.instructions.length}})
 })
 
+router.post("/", (req, res) => {
+    const newRecipe = {
+        name: req.body.name,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions
+    }
 
+    const duplicate = data.recipes.find((item) => item.name === newRecipe.name)
+    if(duplicate){
+        res.status(400).json({error: "Recipe already exists"})
+    } else {
+        let newRecipeData = JSON.stringify(newRecipe, null, 2);
+        const fileData = JSON.parse(fs.readFileSync('data.json'))
+        fileData.recipes.push(JSON.parse(newRecipeData))
+        fs.writeFileSync('data.json', JSON.stringify(fileData, null, 2));
+        res.status(201).json("new recipe added!" + newRecipeData)
+    }
+    
+})
 
 module.exports = router;
